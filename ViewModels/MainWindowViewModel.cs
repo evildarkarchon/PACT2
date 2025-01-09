51a3970dac8353581a1e568ca17366921b767711
evildarkarchon <evildarkarchon@gmail.com>
2025-01-09 03:08:59 -0800

    Starting over part 3

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace PACT2.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace PACT2.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
